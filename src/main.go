d3f509d72331cc9e09c15287f1bd6558339884d1
Simon Dann <simon.dann@gmail.com>
2016-08-07 21:56:15 +0000

    Adding more functionality

--- src/main.go
+++ src/main.go
@@ -4,16 +4,21 @@ import (
     "fmt"
     "bufio"
     "regexp"
+    "strings"
+    "time"
     "os"
 )
 
 type LogReader struct{
     Users []User
     TotalLines uint
+    LastGenerated int64
     FirstSeen string
     LastSeen string
     RegexAction *regexp.Regexp
     RegexMessage *regexp.Regexp
+    RegexParseAction *regexp.Regexp
+    RegexParseMessage *regexp.Regexp
 }
 
 func (lr *LogReader) LoadFile(path string) bool {
@@ -31,27 +36,73 @@ func (lr *LogReader) LoadFile(path string) bool {
     // Loop over all found lines and analyse them
     for scanner.Scan() {
         line := scanner.Text()
-        lr.ParseLine(line)
+
+        if lr.RegexAction.MatchString(line) == true {
+            lr.ParseLine(line, true)            
+        } else if lr.RegexMessage.MatchString(line) == true {
+            lr.ParseLine(line, false)
+        } else {
+            fmt.Printf("error reading line [%i]\n", lr.TotalLines)
+        }
         lr.TotalLines++
     }
     return true;
 }
 
-func (lr *LogReader) ParseLine(line string) {
+func (lr LogReader) ParseTime(inputDate string) time.Time {
+    // Convert timestamp into unix timestamp
+    // @todo make input format configurable
+    lineTime, err := time.Parse("2006-01-02 15:04:05 -0700", inputDate)
+    if err != nil {
+        panic(err)
+    }
+    return lineTime
+}
 
-    fmt.Println("Parsing Line: [" + line + "]")
-    fmt.Println("Is action: ", lr.RegexAction.MatchString(line))
-    fmt.Println("Is message: ", lr.RegexMessage.MatchString(line))    
-    fmt.Println("")
+func (lr LogReader) IsUserIgnored(username string) bool {
+    // ...
+}
+
+func (lr *LogReader) ParseLine(line string, isAction bool) bool {
+
+    var parsed[][]string
+
+    fmt.Println("Parsing Line: [" + line + "]\n\n")
+    // timestamp = [0][1]
+    // nick = [0][2]
+    // message/action = [0][3]
+    if isAction == true {
+        parsed = lr.RegexParseAction.FindAllStringSubmatch(line, -1)
+    }else{
+        parsed = lr.RegexParseMessage.FindAllStringSubmatch(line, -1)
+    }
+
+    // Convert timestamp into unix timestamp
+    lineTime := lr.ParseTime(parsed[0][1])
+    if lineTime.Unix() < lr.LastGenerated {
+        return false
+    }
+
+    // Parse nick and check against ignore list
+    lineNick := strings.Trim(parsed[0][2], " ")
+    // @todo check against ignore list
+
+    return true
 }
 
 func main() {
-    lr := LogReader{RegexAction: regexp.MustCompile(`^\[(.+)\] \* (.+)$`), RegexMessage: regexp.MustCompile(`^\[(.+)\] <(.+)> (.+)$`)}
+    lr := LogReader{
+        RegexAction: regexp.MustCompile(`^\[(.+)\] \* (.+)$`),
+        RegexMessage: regexp.MustCompile(`^\[(.+)\] <(.+)> (.+)$`),
+        RegexParseAction: regexp.MustCompile(`^\[(.+)\] \* (\S+) (.+)$`), 
+        RegexParseMessage: regexp.MustCompile(`^\[(.+)\] <(\S+)> (.+)$`),
+    }
+
     lr.LoadFile("irctest.log") 
 
     u := NewUser("Simon", "Today")
-    fmt.Printf("%v\n", lr)
+    //fmt.Printf("%v\n", lr)
 
     u.CalculateTotals()
-    fmt.Printf("%v\n", u)
+    //fmt.Printf("%v\n", u)
 }
