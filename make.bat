74ceea21a73e5193d8b83e05cf99259d96f2182b
Simon Dann <simon.dann@halesway.co.uk>
2016-08-10 11:50:04 +0100

    Made a windows version of the Makefile

---
+++ make.bat
@@ -0,0 +1,36 @@
+@ECHO OFF
+SET BINARY=logstats.exe
+SET VERSION=1.0.0
+for /f "delims=" %%a in ('git rev-parse HEAD') do SET BUILD=%%a
+SET LDFLAGS=-ldflags "-X main.Version=%VERSION% -X main.Build=%BUILD%"
+
+set ARG1=%1
+
+if "%ARG1%" == "clean" (
+    CALL :clean
+) ELSE IF "%ARG1%" == "build" (
+    CALL :build
+) ELSE IF "%ARG1%" == "clean-build" (
+    CALL :clean
+    CALL :build
+) ELSE (
+    ECHO Incorrect arguments passed [%ARG1%]
+)
+EXIT /B 0
+
+:clean
+if exist bin (
+    RD /S /Q "bin"
+)
+EXIT /B 0
+
+:build
+@ECHO ON
+go build %LDFLAGS% -o ./bin/%BINARY%
+@ECHO OFF
+copy "./config.yaml" "./bin/config.yaml" > nul
+copy "./irctest.log" "./bin/irctest.log" > nul
+cd bin
+%BINARY%
+cd ..
+EXIT /B 0
