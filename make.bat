d50c1211fc4245a7922c43254a2dea5e25c81080
Simon Dann <simon.dann@halesway.co.uk>
2016-08-17 16:08:56 +0100

    Make batch file now working - needs porting to Makefile for linux

--- make.bat
+++ make.bat
@@ -1,7 +1,12 @@
 @ECHO OFF
 
+REM How to use:
+REM To begin with execute make build && make copy && make generate-log
+REM This will initiate the logstats folder in the bin directory. Afterwards you can then
+REM run make build && make run to build and then execute from source.
+
 REM Set executable name
-SET PACKAGENAME=logstats.exe
+SET PACKAGENAME=main.exe
 
 REM Set GOBIN
 SET GOBIN=%GOPATH%\bin\logstats
@@ -12,7 +17,7 @@ for /f "delims=" %%a in ('git rev-parse HEAD') do SET BUILD=%%a
 SET LDFLAGS=-ldflags "-X main.Version=%VERSION% -X main.Build=%BUILD%"
 
 REM Set current working directory
-SET CWD=%cd%\
+SET CWD=%cd%
 
 REM Get first argument passed by command line
 set ARG1=%1
@@ -32,10 +37,19 @@ if "%ARG1%" == "clean" (
 ) ELSE IF "%ARG1%" == "update" (
     echo Updating dependencies
     CALL :update
+) ELSE IF "%ARG1%" == "run" (
+    CALL :run
+) ELSE IF "%ARG1%" == "build-test-log" (
+    echo Building Test Log
+    CALL :generate-log
 ) ELSE (
     ECHO Incorrect arguments passed [%ARG1%]
 )
 
+EXIT /B 0
+
+:run
+%GOBIN%\%PACKAGENAME% -d %GOBIN%
 EXIT /B 0
 
 :update
@@ -50,12 +64,8 @@ EXIT /B 0
 
 :build
 @ECHO ON
-go install %LDFLAGS% %CWD%cmd\ircstats\main.go
+go install %LDFLAGS% %CWD%\cmd\ircstats\main.go
 @ECHO OFF
-EXIT /B 0
-
-:run
-bin\%BINARY% -d ./bin
 EXIT /B 0
 
 :copy
@@ -66,5 +76,5 @@ cd ..
 EXIT /B 0
 
 :generate-log
-php extra\createtestlog.php > .\bin\irctest.log
+php %CWD%\extra\createtestlog.php > %GOBIN%\irctest.log
 EXIT /B 0
