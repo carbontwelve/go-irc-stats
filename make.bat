8c204a20a1fc8a4236a9908c7b5e61668ea6eb56
Simon Dann <simon.dann@halesway.co.uk>
2016-08-17 15:59:10 +0100

    Make make.bat work with $GOPATH

--- make.bat
+++ make.bat
@@ -1,47 +1,68 @@
 @ECHO OFF
-SET BINARY=logstats.exe
+
+REM Set executable name
+SET PACKAGENAME=logstats.exe
+
+REM Set GOBIN
+SET GOBIN=%GOPATH%\bin\logstats
+
+REM Set ld flags containing current version and git hash
 SET VERSION=1.0.0
 for /f "delims=" %%a in ('git rev-parse HEAD') do SET BUILD=%%a
 SET LDFLAGS=-ldflags "-X main.Version=%VERSION% -X main.Build=%BUILD%"
 
+REM Set current working directory
+SET CWD=%cd%\
+
+REM Get first argument passed by command line
 set ARG1=%1
 
+REM Do action if found
 if "%ARG1%" == "clean" (
+    echo Cleaning Build Directory
     CALL :clean
+) ELSE IF "%ARG1%" == "copy" (
+    echo Copying extra files
+    CALL :copy
 ) ELSE IF "%ARG1%" == "build" (
+    echo Building Project
     CALL :build
-) ELSE IF "%ARG1%" == "generate-log" (
-    CALL :generate-log
-) ELSE IF "%ARG1%" == "run" (
-    CALL :run
-) ELSE IF "%ARG1%" == "clean-build" (
-    CALL :clean
-    CALL :build
+    echo Copying extra files
+    CALL :copy
+) ELSE IF "%ARG1%" == "update" (
+    echo Updating dependencies
+    CALL :update
 ) ELSE (
     ECHO Incorrect arguments passed [%ARG1%]
 )
+
+EXIT /B 0
+
+:update
+go get -v ./...
 EXIT /B 0
 
 :clean
-if exist bin (
-    RD /S /Q "bin"
+if exist %GOBIN% (
+    RD /S /Q %GOBIN%
 )
 EXIT /B 0
 
 :build
 @ECHO ON
-cd cmd\ircstats
-go get -t -v ./...
-go build %LDFLAGS% -o ..\..\bin\%BINARY%
+go install %LDFLAGS% %CWD%cmd\ircstats\main.go
 @ECHO OFF
-cd ..\..\extra
-copy "config.yaml" "..\bin\config.yaml" > nul
-copy "template.html" "..\bin\template.html" > nul
-cd ..
 EXIT /B 0
 
 :run
 bin\%BINARY% -d ./bin
+EXIT /B 0
+
+:copy
+cd extra
+copy "config.yaml" "%GOBIN%\config.yaml" > nul
+copy "template.html" "%GOBIN%\template.html" > nul
+cd ..
 EXIT /B 0
 
 :generate-log
