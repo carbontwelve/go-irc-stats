4fff45c4eda6cd6c7d1626f1470d6e3fc38b3b16
Simon Dann <simon.dann@gmail.com>
2016-08-17 23:41:33 +0100

    Adding ActiveUsers property to Channel struct

--- ircstats/channel.go
+++ ircstats/channel.go
@@ -1,7 +1,7 @@
 package ircstats
 
 type MaxWeek struct {
-	Week  string
+	Week  int64
 	Lines int64
 }
 
@@ -16,25 +16,25 @@ type MaxHour struct {
 }
 
 type Averages struct {
-	Day     float64 	// Average lines by day (365 days)
-	Week    float64 	// Average lines by week (week 1 - week 52)
-	WeekDay float64 	// Average lines by week day (monday - sunday)
+	Day     float64 		// Average lines by day (365 days)
+	Week    float64 		// Average lines by week (week 1 - week 52)
+	WeekDay float64 		// Average lines by week day (monday - sunday)
 }
 
 //
 // Channel Statistics
 //
 type Channel struct {
-	Name      string   	// Channel Name
-	UserCount int64    	// Total Number of users in Channel
-	LineCount int64    	// Total Number of lines in Channel
-	WordCount int64    	// Total Word count for Channel
-	MaxDay    MaxDay   	// Calculated Max Day
-	MaxHour   MaxHour  	// Calculated Max Hour
-	MaxWeek   MaxWeek  	// Calculated Max Week
-	Averages  Averages 	// Calculated Averages
-	HoursAndDaysStats	// Inherited Hour And Days methods
-	Seen			// First & Last Seen
+	Name        string   		// Channel Name
+	ActiveUsers map[string]User	// Active users in past 30 days
+	UserCount   int64    		// Total Number of users in Channel
+	LineCount   int64    		// Total Number of lines in Channel
+	WordCount   int64    		// Total Word count for Channel
+	MaxDay      MaxDay   		// Calculated Max Day
+	MaxHour     MaxHour  		// Calculated Max Hour
+	MaxWeek     MaxWeek  		// Calculated Max Week
+	Averages    Averages 		// Calculated Averages
+	HoursAndDaysStats    		// Inherited Hour And Days methods
+	Seen                 		// First & Last Seen
 }
-
 
